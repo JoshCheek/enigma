ed87d7d09fab75eb2166739bcb716ef023a487f6
Josh Cheek <josh.cheek@gmail.com>
2015-06-05 03:17:44 -0600

    Refactor Enigma#encrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -18,38 +18,40 @@ class Enigma
     letters + numbers + others
   end
 
-  def initialize(unencrypted_message, key, date)
-    @unencrypted_message = unencrypted_message
-    @key                 = key
-    @date                = date
+
+
+  def initialize(message, key, date, map=Enigma.default_character_map)
+    @map       = map
+    @message   = message
+    @key       = key
+    @date      = date
+    @offsets   = (@date.to_i ** 2).to_s[-4..-1].chars.map(&:to_i)
+    @rotations = @key.chars.each_cons(2).to_a.map(&:join).map(&:to_i)
   end
 
   def encrypt
-    # turn my message into chunks
-    chunks = @unencrypted_message.chars.each_slice(4).to_a
-
-    # encrypt each chunk
-    encrypted_chunks = chunks.map { |chunk| encrypt_chunk chunk }
-
-    # join my chunks
-    encrypted_chunks.join
+    chunkify(@message).map { |chunk| encrypt_chunk chunk }.join
   end
 
   private
 
-  def encrypt_chunk(chunk)
-    date_square = (@date.to_i ** 2).to_s
-    offsets     = date_square[-4..-1]
+  attr_reader :map, :offsets
 
-    chunk.map.with_index { |char, i|
-      encrypt_char char, @key[i..i+1].to_i, offsets[i].to_i
-    }
+  def chunkify(message)
+    message.chars.each_slice(4)
+  end
+
+  def encrypt_chunk(chunk)
+    chunk.map.with_index do |char, index|
+      rotation  = @rotations[index]
+      encrypt_char char, rotation, offsets[index]
+    end
   end
 
   def encrypt_char(character, rotation, offset)
-    unencrypted_index = Enigma.default_character_map.index character
+    unencrypted_index = map.index character
     encrypted_sum     = unencrypted_index + rotation + offset
-    encrypted_index   = encrypted_sum % Enigma.default_character_map.length
-    Enigma.default_character_map[encrypted_index]
+    encrypted_index   = encrypted_sum % map.length
+    map[encrypted_index]
   end
 end
