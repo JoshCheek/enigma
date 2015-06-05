b3aa004c2f6f7b183f0057a7c9e60bf075e743b1
Josh Cheek <josh.cheek@gmail.com>
2015-06-04 20:46:51 -0600

    Wire the world together

---
+++ lib/encrypt.rb
@@ -0,0 +1,19 @@
+require 'what_weve_got_here_is_an_error_to_communicate'
+require 'date'
+require_relative 'enigma'
+
+# read the input file
+input_filename      = ARGV[0]
+unencrypted_message = File.read input_filename
+
+# magic encryptiony shit
+key    = Enigma.random_key
+date   = Date.today.strftime("%d%m%y") # "040615" # today's date, apparently
+enigma = Enigma.new unencrypted_message, key, date
+
+# write the output file
+output_filename = ARGV[1]
+File.write output_filename, enigma.encrypt
+
+# print the file, key, and date
+puts "Created #{output_filename.inspect} with the key #{key} and date #{date}"
---
+++ lib/enigma.rb
@@ -0,0 +1,15 @@
+class Enigma
+  def self.random_key
+    "68403"
+  end
+
+  def initialize(unencrypted_message, key, date)
+    @unencrypted_message = unencrypted_message
+    @key = key
+    @date = date
+  end
+
+  def encrypt
+    "fmotmfzwptg"
+  end
+end
