45fcbcc01cb5185118b9304bd2528f350456d2a7
Josh Cheek <josh.cheek@gmail.com>
2015-06-04 21:23:29 -0600

    The random key is a digit and random

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,10 @@
 class Enigma
   def self.random_key
-    "68403"
+    Array.new(5) { random_character }.join
+  end
+
+  def self.random_character
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].sample
   end
 
   def initialize(unencrypted_message, key, date)
--- test/test_random_key.rb
+++ test/test_random_key.rb
@@ -5,8 +5,9 @@ class TestRandomKey < Minitest::Test
     assert_equal char, char.to_i.to_s
   end
 
-  def test_the_key_is_five_digits_like_41521
+  def test_the_key_is_a_string_of_five_digits_like_41521
     random_key = Enigma.random_key
+    assert_equal String, random_key.class
     assert_equal 5, random_key.length
     assert_digit random_key[0]
     assert_digit random_key[1]
@@ -16,10 +17,14 @@ class TestRandomKey < Minitest::Test
   end
 
   def test_each_key_is_random
-    skip 'implement me!'
+    # if I call it a shitload of times I'l get at least 2 values
+    keys = 1000.times.map { Enigma.random_key }
+    assert 1 < keys.uniq.length
   end
 
   def test_a_digit_is_0_9
-    skip 'implement me!'
+    digits_seen = 1000.times.map { Enigma.random_key }.join.chars
+    all_digits  = digits_seen.uniq.sort.join
+    assert_equal "0123456789", all_digits
   end
 end
